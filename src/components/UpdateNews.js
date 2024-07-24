c3f5b24699223b7e916949ffe78d978c0732f988
Qadar110 <hooyoubax0@gmail.com>
2024-07-24 12:12:29 +0300

    Delete src directory

--- src/components/UpdateNews.js
+++
@@ -1,9 +0,0 @@
-import { getFirestore, doc, updateDoc } from 'firebase/firestore';
-import { db } from '../firebase';
-// Import db from firebase.js
-
-const UpdateNews = ({ id, currentTitle, currentContent, onUpdate }) => {
-  // Your existing code
-};
-
-export default UpdateNews;
